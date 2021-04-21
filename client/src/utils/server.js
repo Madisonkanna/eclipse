6de5be17be189be533570f42d6f85c1e4d0c0cf0
Madison Kanna <madison@mediavine.com>
2021-04-20 18:59:31 -0700

    server updates

--- client/src/utils/server.js
+++ client/src/utils/server.js
@@ -7,25 +7,61 @@ const host = 'http://localhost:3000'
 // example function calling authenticate on a user's username and password, returning its results
 // const authUser = async (email, password) => await fetch('POST', `${host}/auth-user`, [JSON.stringify({ email, password })])
 
+let authToken = undefined
 
-const createUser = ({ email, data, publicKey, privateKey, salt }) => {
-	return fetch('http://localhost:3000/create-user', {
+import {
+	derivePassKeyHash,
+} from './encryption.js'
+
+const _fetch = (route, body = {}) => {
+	console.log(authToken, 'auth token')
+	return fetch(`${host}/${route}`, {
 		method: 'POST',
-		body: JSON.stringify({
-			email, data, publicKey, privateKey, salt
-		}),
+		body: JSON.stringify(body),
 		headers: {
 			'Content-Type': 'application/json',
-			'Access-Control-Allow-Origin': '*'
+			'Access-Control-Allow-Origin': '*', 
+			'authorization': authToken
     	},
 	})
-	console.log('res:', res)
 }
+
+const getUsers = () => _fetch('get-user-data')
+
+const authenticate = async ({ email, password }) => {
+	const getSaltResponse = await _fetch('pre-login', { email })
+	console.log(getSaltResponse, 'get salt response')
+	const saltResponse = await getSaltResponse.json()
+	console.log(saltResponse, 'salt response')
+	if (!saltResponse.salt) {
+		return {
+			success: false
+		}
+	}
+	const salt = saltResponse.salt
+	const { key, hash } = await derivePassKeyHash({ password, salt })
+	const getLoginResponse = await _fetch('login', { email, hash })
+	const loginRes = await getLoginResponse.json()
+	console.log('loginRes:', loginRes)
+	authToken = loginRes.token
+	if (loginRes.success) {
+		const getUsersData = await getUsers()
+		const usersData = await getUsersData.json()
+		console.log(usersData, 'usersdData')
+		return usersData
+
+	}
+	return loginRes
+}
+// Body: { email, data, publicKey, privateKey, salt }
+const createUser = body => _fetch('create-user', body)
 
 const serverApi = {
 	// export fetch utilities here
 	// authUser
-	createUser
+	createUser,
+	authenticate,
+	getUsers
 }
 
 export default serverApi
